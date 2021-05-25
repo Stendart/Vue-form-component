7678e035c31ce26ff15d6955777ef3d73a7db8ea
Stendart <Reiden1998@yandex.ua>
2021-05-25 18:22:53 +0300

    add modal form

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <button class="btn" @click="isShowForm = true">Регистрация</button>
+    <appModal v-if="isShowForm" @close="isShowForm = false">
+      <AppForm></AppForm>
+    </appModal>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import AppModal from './components/AppModal';
+import AppForm from './components/AppForm';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      isShowForm: false
+    }
+  },
+  components: {
+    HelloWorld,
+    AppModal,
+    AppForm
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.btn {
+  color: #42b983;
+  position: relative;
+  place-content: center;
+  place-items: center;
+  width: fit-content;
+  border-radius: 99px;
+  letter-spacing: 0.05em;
+  border: 1px solid #42b983;
+  text-decoration: none;
+  text-transform: uppercase;
+  margin-right: 10px;
+  padding: 0.5rem 1.5rem;
+  white-space: nowrap;
+  font-weight: 700;
+  outline: none;
+  background: #fff;
+  transition: all 0.22s;
+}
+
+.btn:hover {
+  cursor: pointer;
+  opacity: 0.8;
+}
+
+.btn:disabled {
+  cursor: not-allowed;
+  opacity: 1!important;
+  background: #eee!important;
+  border-color: #ddd!important;
+  color: #999!important;
+}
+
+.btn:active {
+  box-shadow: inset 1px 1px 1px rgba(0, 0, 0, 0.3);
+}
+</style>
---
+++ src/components/AppForm.vue
@@ -0,0 +1,124 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <div class="form-control" :class="{'invalid' : $v.fio.$error }">
+            <label for="name">ФИО</label>
+            <input type="text" ref="name"
+                   id="name" v-model="fio"
+                   :class="{'invalid' : $v.fio.$error }">
+<!--            <small >{{$v.fio.required }}</small>-->
+            <small v-if="$v.fio.$dirty && !$v.fio.required"> Обязательное поле </small>
+            <small v-if="$v.fio.$dirty && !$v.fio.email"> Вы ввели некорректный email </small>
+        </div>
+            <br>
+            <br>
+            <br>
+            <hr>
+        <div class="form-control" :class="{'invalid' : $v.fio.$error }">
+            <div class="form-group" :class="{ 'invalid': $v.fio.$error }">
+                <label>Name</label>
+                <input v-model.trim="$v.fio.$model"/>
+            </div>
+            <div class="error" v-if="!$v.fio.required">Field is required</div>
+            <div class="error" v-if="!$v.fio.minLength">Name must have at least {{$v.fio.$params.minLength.min}} letters.</div>
+        </div>
+            <br>
+            <br>
+            <br>
+            <hr>
+            <br>
+            <br>
+            <br>
+        <div class="form-control" :class="{'invalid' : $v.fio.$error }">
+            <label for="phone">Телефон</label>
+            <input type="text" id="phone" v-model="phone">
+<!--            <small v-if="$v.password.$error ">{{$v.password.$error }}</small>-->
+
+        </div>
+<!--            <label for="status">Статус заявки</label>-->
+<!--            <select id="status" v-model="status">-->
+<!--                <option value="done">Завершен</option>-->
+<!--                <option value="cancelled">Отменён</option>-->
+<!--                <option value="active">Активен</option>-->
+<!--                <option value="pending">Выполняется</option>-->
+<!--            </select>-->
+
+            <button type="submit" class="btn primary">Отправить</button>
+<!--        </div>-->
+    </form>
+</template>
+
+<script>
+  import { required, minLength, between, email } from 'vuelidate/lib/validators'
+
+  export default {
+    name: "AppForm",
+    data() {
+      return {
+        fio: '',
+        phone: null,
+        status: ''
+      }
+    },
+    validations: {
+      fio: {
+        required,
+        email,
+        minLength: minLength(4)
+      },
+      phone: {
+        minLength: minLength(6)
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('Submit')
+        console.log(this.$v.fio.$dirty)
+
+        if (this.$v.$invalid) {
+          this.$v.$touch()
+          return
+        }
+      },
+      submit() {
+        console.log('submit!')
+        this.$v.$touch()
+        if (this.$v.$invalid) {
+          this.submitStatus = 'ERROR'
+        } else {
+          // do your submit logic here
+          this.submitStatus = 'PENDING'
+          setTimeout(() => {
+            this.submitStatus = 'OK'
+          }, 500)
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+    .form-control input,
+    .form-control select,
+    .form-control textarea{
+        font-family: Inter, Roboto, Oxygen, Fira Sans, Helvetica Neue, sans-serif;
+        margin: 0;
+        outline: none;
+        /*border: 2px solid #ccc;*/
+        display: block;
+        width: 90%;
+        color: #2c3e50;
+        padding: 0.5rem 1.5rem;
+        border-radius: 3px;
+        font-size: 1rem;
+        resize: none;
+    }
+
+    .form-control small {
+        color: #e53935;
+    }
+
+    .invalid {
+        border-color: #e53935;
+    }
+</style>
